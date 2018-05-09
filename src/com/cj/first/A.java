af3af74a9865c5eb21c440f96634b4e3c373c7e9
caoju <362204801@qq.com>
2018-05-09 17:36:40 +0800

    fffffffffffff

--- src/com/cj/first/A.java
+++ src/com/cj/first/A.java
@@ -1,4 +1,14 @@
 package com.cj.first;
 
 public class A {
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
